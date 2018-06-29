8e24e2131ac6a809b3836d94c844d800629f77c3
c-daniel14 <cojocarugeani@yahoo.com>
2018-06-29 22:41:21 +0100

    functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,13 +25,32 @@ guessBtn.addEventListener("click", () => {
     }
     // Check if win
     if(guess === winningNum) {
+        // Game over - won
         // Disable input 
         guessInput.disabled = true;
         // Change border color
         guessInput.style.borderColor = "green";
         setMessage(`${winningNum} is correct! You win!`, "green");
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if(guessesLeft === 0){
+            // Game over - lost
+
+            // Disable input 
+            guessInput.disabled = true;
+            // Change border color
+            guessInput.style.borderColor = "red";
+            setMessage(`Game Over, you lost. The correct number was ${winningNum}`, "red");
+        } else {
+            //  Game continues - answer wrong
+
+            // Clear input
+            guessInput.value = "";
+            // Tell user it's the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+        }
     }
     
 })
